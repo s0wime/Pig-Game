74a0ec34eb3dac0c5b4a7db292e24fda979a6fb7
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-13 18:48:34 +0200

    write dice image changing

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceEl.classList.add('hidden');
+
+function generateDice() {
+  return Math.trunc(Math.random() * 6) + 1;
+}
+
+btnRoll.addEventListener('click', function () {
+  const dice = generateDice();
+  diceEl.classList.remove('hidden');
+  diceEl.src = `images/dice-${dice}.png`;
+});
