f75cd28fc8978e75ae322cde14681db32733f8d9
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-14 22:34:04 +0200

    add the logic for holding the scores

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,15 +16,23 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 diceEl.classList.add('hidden');
 
 function generateDice() {
   return Math.trunc(Math.random() * 6) + 1;
 }
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
 
 btnRoll.addEventListener('click', function () {
   const dice = generateDice();
@@ -34,8 +44,24 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .getElementById(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    return;
+  }
+
+  switchPlayer();
 });
