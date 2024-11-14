e73cba2a734d8e853dca950586deae1ad3798754
s0wime <pokhodun.vitalii@lll.kpi.ua>
2024-11-14 22:02:57 +0200

    write adding dice to the current score

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 
 const btnNew = document.querySelector('.btn--new');
@@ -17,8 +20,15 @@ function generateDice() {
   return Math.trunc(Math.random() * 6) + 1;
 }
 
+let currentScore = 0;
+
 btnRoll.addEventListener('click', function () {
   const dice = generateDice();
   diceEl.classList.remove('hidden');
   diceEl.src = `images/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore + dice;
+    current0El.textContent = currentScore;
+  }
 });
